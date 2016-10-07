f32e2267e12a293239fb2e0f0e932d2accc3498c
Steven Burris <stevenburris.jr@gmail.com>
2016-10-07 14:38:56 -0400

    Working on displaying messages;

--- src/com/theironyard/Message.java
+++ src/com/theironyard/Message.java
@@ -4,20 +4,20 @@ package com.theironyard;
  * Created by stevenburris on 10/5/16.
  */
 public class Message {
-    String theMessage;
+    String text;
 
     public Message() {
     }
 
     public Message(String theMessage) {
-        this.theMessage = theMessage;
+        this.text = theMessage;
     }
 
     public String getTheMessage() {
-        return theMessage;
+        return text;
     }
 
     public void setTheMessage(String theMessage) {
-        this.theMessage = theMessage;
+        this.text = theMessage;
     }
 }

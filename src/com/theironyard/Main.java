ecd4c263bbe1e208e0ce23de0385edd52ffe3e8c
Steven Burris <stevenburris.jr@gmail.com>
2016-10-07 20:58:03 -0400

    Working on displaying messages;

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -25,7 +25,7 @@ public class Main {
                     HashMap m = new HashMap();
                     if (user != null) {
                         m.put("name", user.name);
-//                        m.put("name",user.messages);
+                        m.put("userMessages",user.messages);
                     }
                     return new ModelAndView(m, "home.html");
                 },
